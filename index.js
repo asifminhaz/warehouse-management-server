653ad91af9e48e6f307b25a33e1ace95b612d07f
asifminhaz <minhazurrahmanasif40@gmail.com>
2022-05-02 17:34:52 +0600

    add erver

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express()
+const port = process.env.PORT || 5000;
+
+
+//middleware//
+app.use(cors())
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fnywz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('motodeal connected')
+  // perform actions on the collection object
+  client.close();
+});
+
+app.get('/', (req, res)=> {
+          res.send('running the servers')
+});
+
+
+
+
+
+
+
+app.listen(port, () => {
+          console.log('listening to port', port)
+})
