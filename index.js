6f6f618f5e5d6ed69747eb964c6d506028ef6198
asifminhaz <minhazurrahmanasif40@gmail.com>
2022-05-06 11:04:45 +0600

    load inventories

--- index.js
+++ index.js
@@ -1,34 +1,45 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+ const res = require('express/lib/response');
 const app = express()
 const port = process.env.PORT || 5000;
-
 
 //middleware//
 app.use(cors())
 app.use(express.json())
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fnywz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('motodeal connected')
-  // perform actions on the collection object
-  client.close();
-});
+// console.log( process.env.DB_USER, process.env.DB_PASS)
 
+async function run (){
+          try{
+              await client.connect();
+              const inventoriesCollection = client.db('motoDeal').collection('inventories');
+              app.get('/inventories', async (req, res)=> {
+                    const query = {};
+                    const cursor = inventoriesCollection.find(query);
+                    const allInventories = await cursor.toArray();
+                    res.send(allInventories);
+              })
+
+              app.get('/inventories/:id', async (req, res)=> {
+                        const id = req.params.id;
+                        const query = {_id: ObjectId(id) }
+                        const inventories = await inventoriesCollection.findOne(query)
+                        res.send(inventories)
+              })
+          }
+          finally{
+
+          }
+}
+run().catch(console.dir)
 app.get('/', (req, res)=> {
           res.send('running the servers')
 });
-
-
-
-
-
-
 
 app.listen(port, () => {
           console.log('listening to port', port)
