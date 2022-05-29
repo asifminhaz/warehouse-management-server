5a3f7f804b38764b5692231d19482793aa78b9aa
asifminhaz <minhazurrahmanasif40@gmail.com>
2022-05-30 01:58:23 +0600

    add delete api

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
- const res = require('express/lib/response');
+const { MongoClient, ServerApiVersion, ObjectId, ObjectID } = require('mongodb');
+
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -12,7 +12,6 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fnywz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// console.log( process.env.DB_USER, process.env.DB_PASS)
 
 async function run (){
           try{
@@ -23,6 +22,7 @@ async function run (){
                     const cursor = inventoriesCollection.find(query);
                     const allInventories = await cursor.toArray();
                     res.send(allInventories);
+                    console.log('db connected')
               })
 
               app.get('/inventories/:id', async (req, res)=> {
@@ -31,6 +31,48 @@ async function run (){
                         const inventories = await inventoriesCollection.findOne(query)
                         res.send(inventories)
               })
+
+            //   delete inventories
+
+            app.delete('/inventories/:id', async (req, res) => {
+                const id = req.params.id;
+                const query = {_id: ObjectId(id)}
+                const result = await inventoriesCollection.deleteOne(query)
+                res.send(result)
+            })
+
+            // update Quantity to increase
+    app.put('/inventories/:id', async (req, res) => {
+        const id = req.params
+        const body = req.body
+        const filter = {_id: ObjectId(id) }
+        const options = { upsert: true };
+        const update = {
+          $set: {
+            qauantity: body.newQuantity
+  
+          }
+        }
+        const result = await inventoriesCollection.updateOne(filter, update, options);
+        res.send({ success: 'Your Deliver was SuccessFul' })
+      })
+    // update Quentity to decrease
+    app.put('/inventory/:id', async (req, res) => {
+        const id = req.params
+       
+        const body = req.body
+       
+        const filter = {_id: ObjectId(id) }
+        const options = { upsert: true };
+        const update = {
+          $set: {
+            qauantity: body.newQuantity
+  
+          }
+        }
+        const result = await inventoriesCollection.updateOne(filter, update, options);
+        res.send({ success: 'Your added  SuccessFully' })
+      })
           }
           finally{
 
@@ -44,3 +86,5 @@ app.get('/', (req, res)=> {
 app.listen(port, () => {
           console.log('listening to port', port)
 })
+
+
